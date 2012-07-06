fbb52dc6e2ffb059f5b6c1ce68997bd1f6511595
migal-drew <migal.drew@gmail.com>
2012-07-06 23:53:52 +0400

    Integral matrix and rectangle sum

---
+++ SimpleSURF/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SimpleSURF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            /*
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+            string pathToFile = openFileDialog.FileName;
+            
+            Bitmap btmp = new Bitmap(pathToFile);
+
+            double[,]  ar = IntegImage.arrayFromBitmap(btmp);
+            */
+
+            double [,] ar = new double[,] {{1, 3, 4, 2, 5}, {7, 1, 2, 3, 4}, {2, 3, 5, 1, 2}};
+
+            IntegImage i = new IntegImage(ar);
+
+            double[,] res = i.m_matrix;
+
+            double ans = i.getRectSum(0, 0, 3, 3);
+            ans = i.getRectSum(0, 0, 1, 1);
+            ans = i.getRectSum(0, 0, 3, 1);
+            ans = i.getRectSum(0, 0, 5, 5);
+            ans = i.getRectSum(0, 0, 2, 2);
+            ans = i.getRectSum(1, 1, 4, 2);
+
+            IntegImage ghug = new IntegImage(ar);
+        }
+    }
+}
---
+++ SimpleSURF/IntegImage.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace SimpleSURF
+{
+    public class IntegImage
+    {
+        public double[,] m_matrix;
+
+        public double this[int r, int c]
+        {
+            get
+            { 
+                return m_matrix[r, c]; 
+            }
+            set 
+            { 
+                m_matrix[r, c] = value;
+            }
+        }
+
+        public IntegImage(int width, int height)
+        {
+            m_matrix = new double[width, height];
+        }
+
+        public IntegImage(double[,] ar)
+        {
+            int width = ar.GetLength(1);
+            int height = ar.GetLength(0);
+            m_matrix = new double[height, width];
+
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    double val = ar[i, j];
+                    double a = 0, b = 0, c = 0;
+
+                    if (i - 1 >= 0 && j - 1 >= 0)
+                        a = m_matrix[i - 1, j - 1];
+                    if (j - 1 >= 0)
+                        b = m_matrix[i, j - 1];
+                    if (i - 1 >= 0)
+                        c = m_matrix[i - 1, j];
+
+                    m_matrix[i, j] = val - a + b + c;
+                }
+        }
+
+        public static double[,] arrayFromBitmap(Bitmap b)
+        {
+            
+            int width = b.Width;
+            int height = b.Height;
+            double[,] pixels = new double[height, width];
+
+            for (int i = 0; i < height; i++)
+                for (int j = 0; j < width; j++)
+                {
+                    Color c = b.GetPixel(j, i);
+                    double v = (c.R + c.B + c.G) / (3.0 * 255.0);
+
+                    pixels[i, j] = v;
+                }
+
+            return pixels;
+        }
+
+        /// <summary>
+        /// Sum of rectangle. Left top point of rectangle,
+        /// it's width and height
+        /// </summary>
+        /// <param name="col"></param>
+        /// <param name="row"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public double getRectSum(int row, int col, int width, int height)
+        {
+            double a = 0, b = 0, c = 0, d = 0;
+
+            //Left top point of rectangle is first
+            width -= 1;
+            height -=1;
+
+            if (row - 1 >= 0 && col - 1 >= 0) 
+                a = m_matrix[row - 1, col - 1];
+            if (row - 1 >= 0 && col + width < m_matrix.GetLength(1))
+                b = m_matrix[row - 1, col + width];
+            if (row + height < m_matrix.GetLength(0)
+                && col + width < m_matrix.GetLength(1) )
+                c = m_matrix[row + height, col + width];
+            if (row + height < m_matrix.GetLength(0) && col - 1 >= 0)
+                d = m_matrix[row + height, col - 1];
+
+            return a + c - b - d; 
+        }
+    }
+
+    
+}
